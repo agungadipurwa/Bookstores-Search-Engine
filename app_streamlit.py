219606dd2526c51fce1c0385de994b6a21ae9d2d
Agung Adipurwa <74081060+Narazura@users.noreply.github.com>
2021-12-16 10:30:16 +0800

    Update web apps for provide handling value error

--- app_streamlit.py
+++ app_streamlit.py
@@ -9,24 +9,38 @@ st.set_page_config(
         initial_sidebar_state="expanded",)
 
 def main():
-    st.markdown("<h1 style='text-align: center; color: #31333F;'>Gramedia Search Book</h1>",
+    #31333F for Light Theme
+    #FFFFFF for Dark Theme
+    st.markdown("<h1 style='text-align: center; color: #FFFFFF;'>Gramedia Search Book</h1>",
                 unsafe_allow_html=True)
     with st.form(key = 'Search From',clear_on_submit=False):
         nav1,nav2,nav3 = st.columns([1,5,1])
         with nav1:
             choice_term = st.selectbox(label= 'Options',options=['Title','Author','Book Id'])
         with nav2:
-            search_term = st.text_input(label = 'Search by {}'.format(choice_term.lower()))
+            search_term = st.text_input(label = 'Search by')
         with nav3:
             st.text("Search")
             submit_search = st.form_submit_button(label='Go')
-    if submit_search:
-        #this code will use for get data after search and create them into dataframe
-        df = select(choice_term, search_term)
-        st.success("Your searched for '{}' in {}".format(search_term, choice_term))
-        st.table(df)
-    elif submit_search == True and search_term == "":
+
+    if submit_search == True and search_term == "":
         st.error("No data has input")
+
+    elif submit_search:
+        #Check the input match or not, if you input an book id as string you will get value error
+        try:
+            df = select(choice_term, search_term)
+        #handling value error after input book id as string
+        except ValueError:
+            if choice_term == "Book Id":
+                try:
+                    print(int(search_term))
+                except:
+                    st.error("You only can search a Book Id using a number")
+        else:
+            df = select(choice_term, search_term)
+            st.success("Your searched for {} in {}".format(search_term, choice_term))
+            st.table(df)
     
 if __name__ == "__main__":
     main()
