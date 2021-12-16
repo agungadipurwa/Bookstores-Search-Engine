481ccbe07d7e06b19acb0f96db197258831047ae
Agung Adipurwa <74081060+Narazura@users.noreply.github.com>
2021-12-16 15:28:31 +0800

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -16,7 +16,7 @@ def main():
     with st.form(key = 'Search From',clear_on_submit=False):
         nav1,nav2,nav3 = st.columns([1,5,1])
         with nav1:
-            choice_term = st.selectbox(label= 'Options',options=['Title','Author','Book Id'])
+            choice_term = st.selectbox(label= 'Options',options=['Title','Author','Book ID'])
         with nav2:
             search_term = st.text_input(label = 'Search by')
         with nav3:
@@ -24,7 +24,7 @@ def main():
             submit_search = st.form_submit_button(label='Go')
 
     if submit_search == True and search_term == "":
-        st.error("No data has input")
+        st.error("There aren't input data")
 
     elif submit_search:
         #Check the input match or not, if you input an book id as string you will get value error
@@ -32,11 +32,11 @@ def main():
             df = select(choice_term, search_term)
         #handling value error after input book id as string
         except ValueError:
-            if choice_term == "Book Id":
+            if choice_term == "Book ID":
                 try:
                     print(int(search_term))
                 except:
-                    st.error("You only can search a Book Id using a number")
+                    st.error("You only can search a Book ID using a number")
         else:
             df = select(choice_term, search_term)
             st.success("Your searched for {} in {}".format(search_term, choice_term))
